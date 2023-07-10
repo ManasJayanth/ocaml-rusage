290fac7094aba4a301be02096ed1f76e61631602
Manas Jayanth <3097018+ManasJayanth@users.noreply.github.com>
2023-07-10 16:20:40 +0530

    WIP: windows implementation for rusage

--- src/rusage_stubs.c
+++ src/rusage_stubs.c
@@ -19,7 +19,63 @@ PERFORMANCE OF THIS SOFTWARE. */
 #include <caml/memory.h>
 #include <caml/mlvalues.h>
 
+#ifndef _WIN32
 #include <sys/resource.h>
+#else
+#include <windows.h>
+#include <errno.h>
+#include "resource.h"
+#include <time.h>
+#include <psapi.h>
+#include <strsafe.h>
+
+static void
+usage_to_timeval(FILETIME *ft, struct timeval *tv)
+{
+    ULARGE_INTEGER time;
+    time.LowPart = ft->dwLowDateTime;
+    time.HighPart = ft->dwHighDateTime;
+
+    tv->tv_sec = time.QuadPart / 10000000;
+    tv->tv_usec = (time.QuadPart % 10000000) / 10;
+}
+
+int
+getrusage(int who, struct rusage *usage)
+{
+    FILETIME creation_time, exit_time, kernel_time, user_time;
+    PROCESS_MEMORY_COUNTERS pmc;
+
+    memset(usage, 0, sizeof(struct rusage));
+
+    if (who == RUSAGE_SELF) {
+        if (!GetProcessTimes(GetCurrentProcess(), &creation_time, &exit_time,
+                             &kernel_time, &user_time)) {
+            return -1;
+        }
+
+        if (!GetProcessMemoryInfo(GetCurrentProcess(), &pmc, sizeof(pmc))) {
+            return -1;
+        }
+
+        usage_to_timeval(&kernel_time, &usage->ru_stime);
+        usage_to_timeval(&user_time, &usage->ru_utime);
+        usage->ru_majflt = pmc.PageFaultCount;
+        usage->ru_maxrss = pmc.PeakWorkingSetSize / 1024;
+        return 0;
+    } else if (who == RUSAGE_THREAD) {
+        if (!GetThreadTimes(GetCurrentThread(), &creation_time, &exit_time,
+                            &kernel_time, &user_time)) {
+            return -1;
+        }
+        usage_to_timeval(&kernel_time, &usage->ru_stime);
+        usage_to_timeval(&user_time, &usage->ru_utime);
+        return 0;
+    } else {
+        return -1;
+    }
+}
+#endif
 #include <sys/time.h>
 
 #define Nothing Val_int(0)
